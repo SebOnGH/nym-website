09924dc03d58704ba78e49b55a67b0de244c44a2
root <root@DESKTOP-4PJCEIV.localdomain>
2023-03-26 10:23:18 +0200

    Added nym-website project

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-Promise.all([  fetch('https://validator.nymtech.net/api/v1/status/mixnode/834/report'),  fetch('https://validator.nymtech.net/api/v1/status/mixnode/834/reward-estimation'),  fetch('https://validator.nymtech.net/api/v1/status/mixnode/834/avg_uptime'),  fetch('https://validator.nymtech.net/api/v1/status/mixnode/834/status'),  fetch('https://validator.nymtech.net/api/v1/status/mixnode/834/stake-saturation')])
-  .then(responses => Promise.all(responses.map(response => response.json())))
+fetch('https://validator.nymtech.net/api/v1/status/mixnode/834/report')
+  .then(response => response.json())
   .then(data => {
-    const resultatsDiv = document.getElementById('resultats');
+    const resultatsDiv = document.getElementById('report-result');
     let html = "";
-    const report = data[0];
-    const rewardEstimation = data[1];
-    const avgUptime = data[2];
-    const status = data[3];
-    const stakeSaturation = data[4];
-    html += `<div class="resultat"><p>mix_id: ${report.mix_id}</p>`;
-    html += `<p>identity: ${report.identity}</p>`;
-    html += `<p>last_day: ${report.last_day}</p></div>`;
-    html += `<div class="resultat"><p>total_node_reward: ${rewardEstimation.total_node_reward}</p>`;
-    html += `<p>operator: ${rewardEstimation.operator}</p>`;
-    html += `<p>delegates: ${rewardEstimation.delegates}</p>`;
-    html += `<p>operating_cost: ${rewardEstimation.operating_cost}</p></div>`;
-    html += `<div class="resultat"><p>avg_uptime: ${avgUptime.avg_uptime}</p></div>`;
-    html += `<div class="resultat"><p>status: ${status.status}</p></div>`;
-    html += `<div class="resultat"><p>saturation: ${parseFloat(stakeSaturation.saturation).toFixed(2)}%</p></div>`;
+    html += `<p>mix_id: ${data.mix_id}</p>`;
+    html += `<p>identity: ${data.identity}</p>`;
+    html += `<p>last_day: ${data.last_day}</p>`;
     resultatsDiv.innerHTML = html;
   })
   .catch(error => {
     console.error('Erreur lors de la récupération des données', error);
   });
+fetch('https://validator.nymtech.net/api/v1/status/mixnode/834/status')
+  .then(response => response.json())
+  .then(data => {
+    const resultatsDiv2 = document.getElementById('status-result');
+    let html = "";
+    html += `<p>status: ${data.status}</p>`;
+    resultatsDiv2.innerHTML = html;
+  })
+  .catch(error => {
+    console.error('Erreur lors de la récupération des données', error);
+  });
+
